31682b2dd0d537a05005ff03a043834b759cd7d9
nissevahavahe <95856641+nissevahavahe@users.noreply.github.com>
2023-11-11 12:40:15 +0200

    First working version 😄

--- generator.py
+++ generator.py
@@ -1,19 +1,27 @@
 import random
-l = int(input('How long should the password be:'))
-b = input('Can it include upper case letters? (Y/N)')
-c = input('Can it include lower case letters? (Y/N)')
-# n = input('Can it include numbers? (Y/N)')
-# s = input('Can it special characters? (Y/N)')
+import string
+l = int(input("\033[92m {}\033[00m" .format("How long should the password be:")))
+b = input("\033[92m {}\033[00m" .format('Can it include upper case letters? (Y/N)'))
+c = input("\033[92m {}\033[00m" .format('Can it include lower case letters? (Y/N)'))
+n = input("\033[92m {}\033[00m" .format('Can it include numbers? (Y/N)'))
+s = input("\033[92m {}\033[00m" .format('Can it special characters? (Y/N)'))
+charList = ""
 r = ""
 
-if l > 0:
-    if b == 'Y':
-        minAscii = 65
-        maxAscii = 90
-    if c == 'Y':
-        maxAscii = 122
-        print(maxAscii)
+if b == 'Y':
+    charList += string.ascii_uppercase
+if c == 'Y':
+    charList += string.ascii_lowercase
+if n == 'Y':
+    charList += string.digits
+if s == 'Y':
+    charList += string.punctuation
+
+if l > 0 and charList != "":
+    cl = len(charList)
     for i in range(l):
-        r += chr(random.randint(minAscii,maxAscii))
+        r += charList[random.randint(0,cl)]
+else:
+    print("\033[91m {}\033[00m" .format("No password was generated! Please provide correct input."))
 
 print(r)
